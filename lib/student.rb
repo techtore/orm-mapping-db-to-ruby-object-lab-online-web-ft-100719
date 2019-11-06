7920cdfd3f2fd219fa6e474bb19ee4e61a44e3f5
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 22:32:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,6 @@ end
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)  
     end
-    # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
