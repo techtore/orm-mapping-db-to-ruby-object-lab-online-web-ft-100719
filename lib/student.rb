64ccfdda00afd15ff00e3900a088ebfa8881f0f1
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 22:57:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,16 @@ end
   end
   
   def self.first_X_students_in_grade_10(x)
+   sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = "10" 
+      LIMIT ?
+    SQL
    
+   DB[:conn].execute(sql, x).map do |row|
+     self.new_from_db(row)
+   end.first
   end
   
   def save
