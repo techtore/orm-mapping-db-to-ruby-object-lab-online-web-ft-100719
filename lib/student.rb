88a6f91d4244993f7351bae59ba83001d11dabb7
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 22:45:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,13 +59,16 @@ end
   end
   
   def self.first_X_students_in_grade_10(num)
+    num.times do 
      sql = <<-SQL
       SELECT * 
       FROM students 
       WHERE grade ="10"
-    SQL
+     SQL
     
-    num.times do DB[:conn].execute(sql).map do |row|
+    end
+    
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row) 
     end
   end
