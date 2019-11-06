4c5d2d9c3e0a741232c49f7593ea7b3d5d49c8bf
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 22:51:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,11 @@ class Student
   attr_accessor :id, :name, :grade
 
  def self.new_from_db(row)
-  new_student = self.new  # self.new is the same as running Song.new
+  new_student = self.new 
   new_student.id = row[0]
   new_student.name =  row[1]
   new_student.grade = row[2]
-  new_student  # return the newly created instance
+  new_student  
 end
 
   def self.all
