7964fbb8f81c2cbfe3aaa26dd46e2f52aa90b2c1
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 22:36:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,18 @@ end
     end
   end
   
+  def self.students_below_12th_grade 
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade < "12"
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row) 
+    end
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
