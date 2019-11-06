cb4d8f1c770b85655123a174406e8393dc52825c
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 22:23:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new(row)
+    
   end
 
   def self.all
